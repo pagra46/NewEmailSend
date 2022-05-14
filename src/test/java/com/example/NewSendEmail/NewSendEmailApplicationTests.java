57ac6b400d7574063502c060da5f566f8ac4c8f6
Pooja Agrawal1 <pooja.agrawal1@ibm.com>
2022-05-14 20:32:15 +0530

    Testing commit

---
+++ src/test/java/com/example/NewSendEmail/NewSendEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NewSendEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSendEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
